b3bb9d8f3359bd555b66a761d61533ab78f195d9
mrtulun <104288816+mrtulun@users.noreply.github.com>
2025-09-13 16:53:54 +0700

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Reverse Proxy with Caddy on Google IDX";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default =
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in pkgs.mkShell {
+        buildInputs = [
+          pkgs.caddy
+        ];
+      };
+  };
+}
